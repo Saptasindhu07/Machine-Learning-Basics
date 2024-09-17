077a0f736e2e3879dc3177b6820ea9f1e9b3c5f1
Sapta Sindhu Palit <126634604+Saptasindhu07@users.noreply.github.com>
2024-09-17 13:12:56 +0530

    9

---
+++ Linear Regression/.ipynb_checkpoints/Custom Poly Regreession-checkpoint.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression,SGDRegressor
+from sklearn.preprocessing import PolynomialFeatures,StandardScaler
+from sklearn.metrics import r2_score
+from sklearn.pipeline import Pipeline
+
+degree=int(input("Enter the Degree: "))
+copy=degree
+X = 6 * np.random.rand(200)
+y=0
+l=[]
+for i in range(degree+1):
+    l.append(X**copy)
+    copy=copy-1
+converted=np.array(l)
+
+coef_real=80*np.random.rand(degree+1)
+
+for i in range(degree+1):
+    y+=coef_real[i]*converted[i]
+
+_l_=[]
+for j in range(len(X)):
+    copy_=degree
+    k=[]
+    for i in range(degree+1):
+        k.append(X[j]**copy_)
+        copy_=copy_-1
+    _l_.append(k)
+converted2=np.array(_l_)
+    
+print(converted2.shape)
+
+lr2=LinearRegression()
+lr2.fit(converted2,y)
+
+print(lr2.predict(converted2))
+
+plt.scatter(X,lr2.predict(converted2),color='r')
+plt.plot(X, y, "b.")
+plt.xlabel("X")
+plt.ylabel("y")
+plt.show()
